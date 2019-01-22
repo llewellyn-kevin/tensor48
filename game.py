1078b023f571975f626e2062d92aedf419caac3d
Kevin Llewellyn <llewellyn.kevin@yahoo.com>
2019-01-21 21:57:11 -0700

    Made a game ready print board function
    Made a print board function that converts all 0's to -'s and prints
    each number in the board raised as a power of two, to better
    resemble 2048

--- game.py
+++ game.py
@@ -8,11 +8,16 @@ RIGHT   = 2
 DOWN    = 3
 UNKNOWN = -1
 
+WIDTH  = 4
+HEIGHT = 4
+
 #define the game data structure and some helper functions
 class Board:
-    def __init__(self):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
         self.rotation_cache = UNKNOWN
-        self.board = self.clear_board()
+        self.board = self._demo_board()
 
     def clear_board(self):
         return np.array([[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]])
@@ -32,11 +37,20 @@ class Board:
             raise AssertionError('''Assertion of Board.undo_rotation is that Board.rotate_board has 
                 been called since this method was last called. This failed. Board.undo_rotation was 
                 either called twice or was called before Board.rotation was ever called''')
+
         self.board = np.rot90(self.board, self.rotation_cache * -1)
         self.rotation_cache = UNKNOWN
-        
+
 
     def print_raw_board(self):
         print(self.board)
+    def print_game_board(self):
+        for i in range(0, self.width):
+            for j in range(0, self.height):
+                scored_val = '-' if (self.board[i][j] == 0) else pow(2, self.board[i][j])
+                print('[{:>4}]'.format(scored_val)),
+            print('') # Line break at the end of each row
 
-board = Board()
+board = Board(WIDTH, HEIGHT)
+board.print_raw_board()
+board.print_game_board()
