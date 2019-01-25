09665a2f8cd5adedaf9e894af3dbe85812ba172d
Kevin Llewellyn <llewellyn.kevin@yahoo.com>
2019-01-24 22:03:51 -0700

    Added logic to display the demo to a human user
    This code displays a template that can be used for the board, but
    is not scalable. This will at least give us a home for testing
    an interface class, between the board and various users, whether
    AI or human

--- demo.py
+++ demo.py
@@ -1,4 +1,56 @@
 import game
+import curses
 
-board = game.Board()
-board.print_game_board()
+continue_game = True
+
+def main(stdscr):
+
+    stdscr.keypad(True)
+    stdscr.nodelay(True)
+
+    stdscr.clear()
+    write_template(stdscr)
+    stdscr.refresh()
+    
+    while continue_game:
+        char = stdscr.getch()
+        if char == ord('q'):
+            break
+        if not char == -1:
+            stdscr.clear()
+            write_template(stdscr)
+            stdscr.refresh()
+
+def write_template(stdscr):
+    stdscr.move(2, 9)
+    stdscr.addstr('2048 in Python')
+    stdscr.move(4, 6)
+    # TODO: Make this display the actual score
+    stdscr.addstr('Current Score: 000000')
+    write_board(stdscr)
+    stdscr.move(24, 2)
+    stdscr.addstr('Press "q" to quit at any time')
+    stdscr.move(0, 0)
+
+# Writes the board to the screen
+# TODO: Make this code scalable with changing board sizes and actually 
+# display numbers
+def write_board(stdscr):
+    for i in range(0, 4):
+        stdscr.move(4 * i + 6, 2)
+        write_border(stdscr)
+        stdscr.move(4 * i + 7, 2)
+        write_line(stdscr)
+        stdscr.move(4 * i + 8, 2)
+        write_line(stdscr)
+        stdscr.move(4 * i + 9, 2)
+        write_line(stdscr)
+    stdscr.move(22, 2)
+    write_border(stdscr)
+def write_line(stdscr):
+    stdscr.addstr('|      |      |      |      |')
+def write_border(stdscr):
+    stdscr.addstr('+------+------+------+------+')
+
+
+curses.wrapper(main)
