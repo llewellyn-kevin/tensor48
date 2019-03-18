0ad94bb8367325f0f45bf8148b87aba43c6b9396
Kevin Llewellyn <llewellyn.kevin@yahoo.com>
2019-03-18 16:21:21 +0000

    Created a TF version of T48Env and added some logging

--- dqn_agent.py
+++ dqn_agent.py
@@ -1,16 +1,29 @@
-
+from dqn_env import T48Env
 
 import tensorflow as tf
 
-from tf_agents.agents.dqn import dqn_agent
-from tf_agents.agents.dqn import q_network
-from tf_agents.drivers import dynamic_step_driver
 from tf_agents.environments import tf_py_environment
-from tf_agents.environments import trajectory
-from tf_agents.metrics import metric_utils
-from tf_agents.metrics import tf_metrics
-from tf_agents.policies import random_tf_policy
-from tf_agents.replay_buffers import tf_uniform_replay_buffer
-from tf_agents.utils import common
+from tf_agents.environments import tf_environment
+
+# from tf_agents.agents.dqn import dqn_agent
+# from tf_agents.agents.dqn import q_network
+# from tf_agents.drivers import dynamic_step_driver
+# from tf_agents.environments import trajectory
+# from tf_agents.metrics import metric_utils
+# from tf_agents.metrics import tf_metrics
+# from tf_agents.policies import random_tf_policy
+# from tf_agents.replay_buffers import tf_uniform_replay_buffer
+# from tf_agents.utils import common
+
+# Hyperparameters
 
 
+
+
+# Set up Environment
+env = T48Env()
+tf_env = tf_py_environment.TFPyEnvironment(env)
+
+print(isinstance(tf_env, tf_environment.TFEnvironment))
+print("TimeStep Specs:", tf_env.time_step_spec())
+print("Action Specs:", tf_env.action_spec())
